916ae0f4fc98fde5b5c3f738d28dc93bf02be5e4
mdsanaullahkhan <adsamikhandu@gmail.com>
2020-08-28 03:01:25 +0600

    Done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+const Header = () => {
+    
+    return (
+        <div className="container header">
+          <a><strong><h1>Online Class </h1></strong></a>
+        </div>
+    );
+};
+
+export default Header;
